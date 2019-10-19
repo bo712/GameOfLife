bee588fc532521f32c2e52d6a61a7d65a74f9db1
Konstantin <linuksoid@gmail.com>
2019-10-19 19:54:25 +0300

    .NET Framework -> .NET Core

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -1,19 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameOfLife
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Field field = new Field(10, 10);
+            var field = new Field(10, 10);
             field.FillArray();
             field.PrintField();
-            Field newField = new Field(10, 10);
+            var newField = new Field(10, 10);
         }
     }
 }
