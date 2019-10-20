df957d431d2ef136b009f654c0edcf2c2f1aeb72
Konstantin <linuksoid@gmail.com>
2019-10-21 01:35:47 +0300

    add AUTOSTOP

--- GameOfLife/Game.cs
+++ GameOfLife/Game.cs
@@ -6,7 +6,7 @@ namespace GameOfLife
     public class Game
     {
         private Field _field;
-        private  Field _newField;
+        private Field _newField;
 
         public Game(int height, int width)
         {
@@ -20,10 +20,41 @@ namespace GameOfLife
             while (true)
             {
                 _field.PrintField();
+
+                if (_field.CalculatePopulation() == 0)
+                {
+                    Console.WriteLine("All dead! All dead!");
+                    break;
+                }
+
                 _field.CheckField(_newField);
+
+                if (CheckGenerationsForEquality())
+                {
+                    Console.WriteLine("AUTOSTOP!");
+                    break;
+                }
+
                 _field = _newField;
-                Thread.Sleep(300);
+                _newField = new Field(_field._array.GetLength(0), _field._array.GetLength(1));
+                Thread.Sleep(50);
             }
+        }
+
+        private bool CheckGenerationsForEquality()
+        {
+            for (int i = 0; i < _field._array.GetLength(0); i++)
+            {
+                for (int j = 0; j < _field._array.GetLength(1); j++)
+                {
+                    if (_field._array[i, j] != _newField._array[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
