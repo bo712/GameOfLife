e49c1b0e0c80a7a7766134d37249e34a46099dd2
Konstantin <linuksoid@gmail.com>
2019-10-21 01:36:23 +0300

    add some testcases in CalculateNeighbours_Points_ReturnRightAnswer()

--- GameOfLife.Tests/FieldTests.cs
+++ GameOfLife.Tests/FieldTests.cs
@@ -7,11 +7,27 @@ namespace GameOfLife.Tests
     {
         [TestCase(0, 0, ExpectedResult = 3)]
         [TestCase(1, 0, ExpectedResult = 4)]
-        [TestCase(2, 0, ExpectedResult = 5)]
-        [TestCase(3, 0, ExpectedResult = 5)]
-        [TestCase(4, 0, ExpectedResult = 3)]
+        [TestCase(2, 0, ExpectedResult = 3)]
+        [TestCase(3, 0, ExpectedResult = 3)]
+        [TestCase(4, 0, ExpectedResult = 0)]
+        [TestCase(0, 5, ExpectedResult = 3)]
+        [TestCase(1, 5, ExpectedResult = 5)]
+        [TestCase(2, 5, ExpectedResult = 5)]
+        [TestCase(3, 5, ExpectedResult = 5)]
+        [TestCase(4, 5, ExpectedResult = 3)]
         [TestCase(0, 1, ExpectedResult = 4)]
-        public int CalculateNeighbours_Point_Ex(int indexV, int indexH)
+        [TestCase(0, 2, ExpectedResult = 5)]
+        [TestCase(0, 3, ExpectedResult = 5)]
+        [TestCase(0, 4, ExpectedResult = 5)]
+        [TestCase(4, 1, ExpectedResult = 3)]
+        [TestCase(4, 2, ExpectedResult = 3)]
+        [TestCase(4, 3, ExpectedResult = 5)]
+        [TestCase(4, 4, ExpectedResult = 5)]
+        [TestCase(1, 1, ExpectedResult = 7)]
+        [TestCase(1, 2, ExpectedResult = 7)]
+        [TestCase(1, 3, ExpectedResult = 7)]
+        [TestCase(1, 4, ExpectedResult = 7)]
+        public int CalculateNeighbours_Points_ReturnRightAnswer(int indexV, int indexH)
         {
             var field = new Field(5, 6)
             {
@@ -20,8 +36,8 @@ namespace GameOfLife.Tests
                     {' ', '*', '*', '*', '*', '*'},
                     {'*', '*', '*', '*', '*', '*'},
                     {'*', '*', '*', ' ', '*', '*'},
-                    {'*', ' ', '*', '*', '*', '*'},
-                    {'*', '*', '*', '*', '*', '*'}
+                    {' ', ' ', '*', '*', '*', '*'},
+                    {'*', ' ', '*', '*', '*', '*'}
                 }
             };
             return field.CalculateNeighbours(indexV, indexH, field._array);
