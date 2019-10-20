6314e45cf603cf4283e08be5c4d99f9fe7f0d3e6
Konstantin <linuksoid@gmail.com>
2019-10-20 16:36:40 +0300

    add Testcases

--- GameOfLife.Tests/FieldTests.cs
+++ GameOfLife.Tests/FieldTests.cs
@@ -5,21 +5,27 @@ namespace GameOfLife.Tests
     [TestFixture]
     public class FieldTests
     {
-        [Test]
-        public void CalculateNeighbours_Point_Ex()
+        [TestCase(0,0,3)]
+        [TestCase(1,0,4)]
+        [TestCase(2,0,5)]
+        [TestCase(3,0,5)]
+        [TestCase(4,0,3)]
+        [TestCase(0,1,4)]
+        public void CalculateNeighbours_Point_Ex(int indexX, int indexY, int expected)
         {
-            var field = new Field(4, 4)
+            var field = new Field(5, 5)
             {
-                Array = new char[,]
+                _array = new char[,]
                 {
-                    {' ', '*', '*', '*'},
-                    {'*', '*', '*', '*'},
-                    {'*', '*', '*', '*'},
-                    {'*', '*', '*', '*'}
+                    {' ', '*', '*', '*', '*'},
+                    {'*', '*', '*', '*', '*'},
+                    {'*', '*', '*', ' ', '*'},
+                    {'*', '*', '*', '*', '*'},
+                    {'*', '*', '*', '*', '*'}
                 }
             };
-            var result = field.CalculateNeighbours(1, 2, field.Array);
-            Assert.AreEqual(7, 7);
+            var result = field.CalculateNeighbours(indexX, indexY, field._array);
+            Assert.AreEqual(expected, result);
         }
     }
 }
